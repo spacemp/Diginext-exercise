0d8638f51d4cfee65e2fe34c3180822467dad7e0
mohammad amirkhani <69719503+spacemp@users.noreply.github.com>
2023-01-11 21:38:10 +0330

    Update tamrin.js

--- tamrin.js
+++ tamrin.js
@@ -1,6 +1,4 @@
 // Q1
-
-
 const functionName = (str, num) => {
     let lengthStr = str.length
     let longStr = Math.ceil(num / lengthStr)
@@ -10,6 +8,47 @@ const functionName = (str, num) => {
     console.log((mainStr.match(new RegExp("a", "g")) || []).length);
 
 }
-
-
 functionName("aba", 10);
+
+// Q2**********************
+function sortAB(str) {
+    const charA = "A"
+    const charB = "B"
+    let letterA = 0;
+    let letterB = 0
+    const length = str.length
+
+    for (let i = 0; i < length; i++) {
+        if (str.charAt(i) == charA) {
+            letterA += 1
+        } else if (str.charAt(i) == charB) {
+            letterB += 1
+        }
+
+        if (letterA == 0) {
+            letterA += 1
+        }
+        if (letterB == 0) {
+            letterB += 1
+        }
+        result = letterA - letterB
+    }
+    console.log(Math.abs(result))
+}
+sortAB("AAA")
+
+// Q3 ***********************
+function swap(arr) {
+    let length = arr.length
+    let minSwap = 0
+    for (i = 0; i < length; i++) {
+        if (arr[i] != i + 1) {
+            let andisArr = arr[i]
+            arr[arr.indexOf(i + 1)] = andisArr
+            arr[i] = i + 1
+            minSwap += 1
+        }
+    }
+    console.log(minSwap)
+}
+swap([5, 4, 3, 1, 2])
